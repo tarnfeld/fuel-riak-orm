7108b9c038b6f0a1d5eef8103e7fce84450e3550
Tom Arnfeld <tarnfeld@me.com>
2012-07-16 22:47:16 +0100

    Initial commit.
    Included git ignore file, riak-php submodule and basic bootstrap.

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Bootstrap file for fuel-riak-orm
+ *
+ * @package fuel-riak-orm
+ * @see http://github.com/tarnfeld/fuel-riak-orm
+ * @author Tom Arnfeld <tarnfeld@me.com>
+ */
+
+Autoloader::add_classes(array(
+
+));
